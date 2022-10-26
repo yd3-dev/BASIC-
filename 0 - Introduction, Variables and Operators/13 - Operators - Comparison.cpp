a798a5b4593905d0b94c986f0a797c31807a574d
yd3-dev <110008601+yd3-dev@users.noreply.github.com>
2022-10-26 14:43:13 +0530

    13 - Operators - Comparison.cpp

---
+++ 0 - Introduction, Variables and Operators/13 - Operators - Comparison.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a(5), b(10);
+    cout << "Comparison Operators :" << endl;
+    cout << "a==b : " << (a==b) << endl;
+    cout << "a!=b : " << (a!=b) << endl;
+    cout << "a<b : " << (a<b) << endl;
+    cout << "a>b : " << (a>b) << endl;
+    cout << "a<=b : " << (a<=b) << endl;
+    cout << "a>=b : " << (a>=b) << endl;
+    return 0;
+}
