95782e0f4421ad79dd4353af69fda18e95189543
yd3-test4 <114435585+yd3-test4@users.noreply.github.com>
2022-10-26 14:23:39 +0530

    Basic Datatypes

---
+++ 0 - Introduction, Variables and Operators/02 - Basic Datatypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age(20);
+    string name("Yash Desai");
+    char gender('M');
+    bool is_married(false);
+    float score(90.91);
+    
+    cout << "Name : " << name << endl;
+    cout << "Age : " << age << endl;
+    cout << "Gender : " << gender << endl;
+    cout << "Married : " << is_married << endl;
+    cout << "Score : " << score << endl;
+    return 0;
+}
