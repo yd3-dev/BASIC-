70ef04604da173ec933cd14f4d91adac713ee1a9
yd3-test5 <114435701+yd3-test5@users.noreply.github.com>
2022-10-26 14:57:36 +0530

    Create 1 - Even Odd with ifelse statements.cpp

---
+++ 1 - Flow Control, Function & Loops/1 - Even Odd with ifelse statements.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n(0);
+    cout << "Enter a number : ";
+    cin >> n;
+    if(n%2) cout << "Number is Odd" << endl;
+    else cout << "Number is Even" << endl;
+    return 0;
+}
